04cf0ae79e9da5f25135fe80e5bc9c3a7d64be47
Louis Rose <mail@louismrose.com>
2024-04-30 12:40:29 +0100

    Adds a couple more high-level unit tests

--- test/facade/test_zamzar_client.py
+++ test/facade/test_zamzar_client.py
@@ -65,6 +65,57 @@ class TestZamzarClient:
         with pytest.raises(NotFoundException):
             zamzar.files.find(job.target_file_ids[0])
 
+    def test_download_and_delete_when_multiple_target_files(self, zamzar, succeeding_multi_output_job_id, tmp_path):
+        """Test that the ZamzarClient can download and delete multiple target files."""
+        output = tmp_path / "output"
+        output.touch()
+
+        assert_empty_file(output)
+
+        job = (
+            zamzar
+            .jobs
+            .find(succeeding_multi_output_job_id)
+            .await_completion()
+            .store(output)
+            .delete_all_files()
+        )
+
+        # Check that a non-empty file has been downloaded
+        assert_non_empty_file(output)
+
+        # Check that the directory contains 3 non-empty pngs
+        pngs = list(tmp_path.glob("*.png"))
+        assert len(pngs) == 3
+        for png in pngs:
+            assert_non_empty_file(png)
+
+        # Check that the source and target files no longer exist
+        with pytest.raises(NotFoundException):
+            zamzar.files.find(job.source_file_id)
+
+        for target_file_id in job.target_file_ids:
+            with pytest.raises(NotFoundException):
+                zamzar.files.find(target_file_id)
+
+    def test_convert_url_with_source_format(self, zamzar, tmp_path):
+        """Test that the ZamzarClient can convert a URL with a source format."""
+        output = tmp_path / "output"
+        output.touch()
+
+        assert_empty_file(output)
+
+        job = zamzar.convert(
+            # URLs containing "unknown" cause a 422 from the mock if filename (i.e., source format) is not supplied
+            source="https://example.org/unknown",
+            source_format="pdf",
+            target_format="txt",
+        )
+
+        # Now download the converted file
+        job.store(output)
+        assert_non_empty_file(output)
+
     def test_can_hit_production(self, api_key):
         """Test that the ZamzarClient returns a welcome message when directed at the production environment."""
         zamzar = ZamzarClient(api_key=api_key, environment=Environment.PRODUCTION)
