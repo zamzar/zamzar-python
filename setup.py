8578d10ca79cf405aa0031cfa6c458ac5cbbac3f
louismrose <louismrose@users.noreply.github.com>
2024-12-03 13:47:57 +0000

    Bump version to v0.0.14

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "zamzar_sdk"
-VERSION = "0.0.13"
+VERSION = "0.0.14"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
